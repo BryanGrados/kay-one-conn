4987b1a2f5e97937775670ed1265ae5a72606966
TNOZamas <bryangrados9@gmail.com>
2023-04-25 16:58:11 -0500

    modified transformData rt

--- src/utils/transformData.js
+++ src/utils/transformData.js
@@ -1,6 +1,8 @@
 const ubigeos = require("@/utils/ubigeos.json");
+const tiendas = require("@/utils/tiendas.json");
 
 export async function transformData(datos_pedido) {
+
 	//Optimizado
 	function formattedDate(date) {
 		return date
@@ -29,7 +31,7 @@ export async function transformData(datos_pedido) {
 
 	//Optimizado
 	function getUbigeo(distrito, provincia, ubigeos) {
-		const ubigeo = ubigeos.find(({ NOMBDIST, NOMBPROV }) => {
+		const ubigeo = ubigeos.find(({ NOMBDIST, NOMBPROV}) => {
 			const cleanDistrito = distrito
 				.trim()
 				.toLowerCase()
@@ -57,6 +59,45 @@ export async function transformData(datos_pedido) {
 		});
 
 		return ubigeo ? ubigeo.UBIGEO : "Ubigeo no encontrado";
+	}
+
+	function isTienda(path) {
+		const direccion = formatText(path[0].datos_envio[0].direccion_envio);
+		const direccion2 = formatText(path[0].datos_envio[0].referencia_envio);
+		const distrito = formatText(path[0].datos_envio[0].distrito);
+		const provincia = formatText(path[0].datos_envio[0].provincia);
+		const departamento = formatText(path[0].datos_envio[0].departamento);
+		const ubigeo = getUbigeo(distrito, provincia, ubigeos);
+
+		const tienda = tiendas.find(({ TIENDA_ID }) => {
+			const cleanDireccion = direccion
+				.trim()
+				.toLowerCase()
+				.normalize("NFD")
+				.replace(/[\u0300-\u036f]/g, "");
+			return cleanDireccion.includes(
+				TIENDA_ID.trim()
+					.toLowerCase()
+					.normalize("NFD")
+					.replace(/[\u0300-\u036f]/g, ""),
+			);
+		});
+		
+		const tienda_nombre = tienda ? formatText(tienda.TIENDA_STRING) : direccion;
+		const tienda_ubigeo = tienda ? formatText(tienda.UBIGEO).replace("PE", "") : ubigeo;
+		const tienda_direccion = tienda ? formatText(tienda.DIRECCION) : direccion2;
+		const tienda_distrito = tienda ? formatText(tienda.DIRECCION_DISTRITO).split(" - ")[0] : distrito;
+		const tienda_provincia = tienda ? formatText(tienda.DIRECCION_DISTRITO).split(" - ")[1] : provincia;
+		const tienda_departamento = tienda ? formatText(tienda.DIRECCION_DISTRITO).split(" - ")[2] : departamento;
+
+		return  {
+			direccion: tienda_nombre,
+			ubigeo: tienda_ubigeo,
+			direccion2: tienda_direccion,
+			distrito: tienda_distrito,
+			provincia: tienda_provincia,
+			departamento: tienda_departamento,
+		}
 	}
 
 	//Optimizado
@@ -99,78 +140,68 @@ export async function transformData(datos_pedido) {
 			datos_pedido.forEach((path) => {
 				const numero_orden = path[0].cabecera_pedido[0].numero_orden;
 				const codigo_orden = "";
-				const fecha_registro = formattedDate(
-					path[0].cabecera_pedido[0].fecha_pedido,
-				);
+				const fecha_registro = formattedDate(path[0].cabecera_pedido[0].fecha_pedido);
 				const fecha_pago = formattedDate(path[0].situacion_pagos[0].fecha_pago);
 				const fecha_nacimiento = "";
-				const email_comprador = formatText(
-					path[0].datos_facturacion[0].email_facturacion,
-				);
-				const mailing_promocional = formatText(
-					path[0].terminos_legales[0].acepta_condiciones,
-				);
-				const nombre_comprador = formatText(
-					path[0].datos_facturacion[0].nombres_facturacion,
-				);
-				const telefono_comprador = fixedNumber(
-					path[0].datos_facturacion[0].telefono_facturacion,
-					0,
-				);
-				const sku = fixedNumber(
-					readArray(path[0].detalle_pedido, "sku"),
-					0,
-					10,
-				);
-				const nombre_producto = readArray(path[0].detalle_pedido, "title").map(
-					(item) => {
-						return formatText(item);
-					},
-				);
+				const email_comprador = formatText(path[0].datos_facturacion[0].email_facturacion);
+				const mailing_promocional = formatText(path[0].terminos_legales[0].acepta_condiciones);
+				const nombre_comprador = formatText(path[0].datos_facturacion[0].nombres_facturacion);
+				const telefono_comprador = fixedNumber(path[0].datos_facturacion[0].telefono_facturacion,0);
+				const total_precio_envio = fixedNumber(path[0].resumen_pedido[0].costo_envio,4);
+				//******************************************//
+				const sku = fixedNumber(readArray(path[0].detalle_pedido, "sku"),0);
+				const nombre_producto = readArray(path[0].detalle_pedido, "title").map((item) => {return formatText(item)});
 				const cantidad = readArray(path[0].detalle_pedido, "quantity_sku");
-				const moneda = path[0].datos_facturacion[0].moneda;
 				const precio_catalogo = readArray(path[0].detalle_pedido, "price");
-				const precio_sale = fixedNumber(
-					readArray(path[0].detalle_pedido, "sale_price"),
-					2,
-					10,
-				);
-				const descuento_catalogo = precio_catalogo.map((precio, index) => {
-					return fixedNumber(precio - precio_sale[index], 2);
-				});
-				const descuento_condicion = 0;
+				const precio_sale = fixedNumber(readArray(path[0].detalle_pedido, "sale_price"),2);
+				const descuento_catalogo = precio_catalogo.map((precio, index) => {return fixedNumber(precio - precio_sale[index], 2)});
 				const precio_unitario = precio_sale;
-				const sub_total = precio_unitario.map((precio, index) => {
-					return fixedNumber(precio * cantidad[index], 2);
-				});
-				const total_precio_envio = fixedNumber(
-					path[0].resumen_pedido[0].costo_envio,
-					2,
-				);
-				const precio_envio = fixedNumber(
-					total_precio_envio / cantidad.length,
-					4,
-				);
-				const venta_total = sub_total.map((precio) => {
-					return fixedNumber(precio + precio_envio, 4);
-				});
-				const gran_total_samishop = path[0].resumen_pedido[0].gran_total;
-				const gran_total = fixedNumber(
-					venta_total.reduce((a, b) => a + b, 0),
-					2,
-				);
-				const precio_total_procesado = venta_total;
+				const sub_total = precio_unitario.map((precio, index) => {return fixedNumber(precio * cantidad[index], 2)});
+				const precio_envio = fixedNumber(total_precio_envio / cantidad.length,4);
+				const venta_total = sub_total.map((precio) => {return fixedNumber(precio + precio_envio, 4)});
 				const total_descuento = descuento_catalogo;
-				const nombre_receptor = formatText(
-					path[0].datos_envio[0].nombres_envio,
-				);
-				const direccion = formatText(path[0].datos_envio[0].direccion_envio);
-				const direccion2 = formatText(path[0].datos_envio[0].referencia_envio);
-				const distrito = formatText(path[0].datos_envio[0].distrito);
-				const provincia = formatText(path[0].datos_envio[0].provincia);
+				const imagen_producto = readArray(path[0].detalle_pedido, "url_imagen_sku");
+				const calcular_porcentaje = (precio, descuento) => {return fixedNumber((descuento * 100) / precio, 2)};
+				const total_sin_igv = venta_total.map((precio) => {
+					const porcentajeIgv = 0.18;
+					const totalSinIgv = precio / (1 + porcentajeIgv);
+					return fixedNumber(totalSinIgv, 4);
+				  });
+				//------------------------------------------//
+				const listadoProductos = sku.map((item, index) => {
+					return {
+						sku: item,
+						nombre_producto: nombre_producto[index],
+						cantidad: cantidad[index],
+						precio_catalogo: precio_catalogo[index],
+						precio_sale: precio_sale[index],
+						descuento_catalogo: descuento_catalogo[index],
+						precio_unitario: precio_unitario[index],
+						sub_total: sub_total[index],
+						precio_envio: precio_envio,
+						venta_total: venta_total[index],
+						total_descuento: total_descuento[index],
+						precio_total_procesado: venta_total[index],
+						imagen_producto: imagen_producto[index],
+						porcentaje_descuento: `${calcular_porcentaje(precio_catalogo[index],descuento_catalogo[index]) || 0}%`,
+						total_sin_igv: total_sin_igv[index],
+					}
+				});
+				//******************************************//
+				const moneda = path[0].datos_facturacion[0].moneda;
+				const descuento_condicion = 0;
+				const gran_total_samishop = path[0].resumen_pedido[0].gran_total;
+				const gran_total = fixedNumber(venta_total.reduce((a, b) => a + b, 0),2);
+				const nombre_receptor = formatText(path[0].datos_envio[0].nombres_envio);
+				//******************************************//
+				const direccion = isTienda(path).direccion;
+				const direccion2 = isTienda(path).direccion2;
+				const distrito = isTienda(path).distrito;
+				const provincia = isTienda(path).provincia;
+				const departamento = isTienda(path).departamento;
+				const ubigeo = isTienda(path).ubigeo;
+				//******************************************//
 				const distrito_provincia = `${distrito} | ${provincia}`;
-				const departamento = formatText(path[0].datos_envio[0].departamento);
-				const ubigeo = fixedNumber(getUbigeo(distrito, provincia, ubigeos), 0);
 				const pais = formatText(path[0].datos_envio[0].pais)
 					.replace(/PERU/g, "PERÚ")
 					.replace(/\n/g, "");
@@ -182,7 +213,7 @@ export async function transformData(datos_pedido) {
 				const ruc = getRuc(path, tipo_comprobante);
 				const razon_social = formatText(getRazonSocial(path, tipo_comprobante));
 				const direccion_facturacion = `${direccion} ${direccion2}`;
-				const distrito_provincia_facturacion = distrito_provincia;
+				const distrito_provincia_facturacion = `${distrito} | ${provincia}`;
 				const departamento_facturacion = departamento;
 				const ubigeo_facturacion = ubigeo;
 				const pais_facturacion = pais;
@@ -193,30 +224,32 @@ export async function transformData(datos_pedido) {
 				const id_pago = path[0].situacion_pagos[0].mo;
 				const numero_documento = getNumeroDocumento(path, ruc);
 				const tipo_documento = getTipoDocumento(tipo_comprobante);
-				const total_sin_igv = venta_total.map((precio) => {
-					return fixedNumber(precio / 1.18, 4);
-				});
-				const porcentaje_cupon = 0;
-				const total_orden = fixedNumber(
-					venta_total.reduce((a, b) => a + b, 0),
-					2,
-				);
-				const total_orden_final = fixedNumber(
-					total_orden - (total_orden - gran_total_samishop),
-					2,
-				);
-				const descuento_cupon = fixedNumber(
-					(total_orden - gran_total_samishop) / cantidad.length,
-					2,
-				);
 
-				const fecha_entrega = "";
-				const pedido_sap = "";
-				const codigo_vendedor = "";
-				const comision_vendedor = "";
-				const servicion_envio = formatText(
-					path[0].datos_envio[0].servicio_envio,
-				);
+				// const final_total_sin_igv = fixedNumber(
+				// 	total_sin_igv.reduce((a, b) => a + b, 0),
+				// 	2,
+				// );
+				// const porcentaje_cupon = 0;
+				// const total_orden = fixedNumber(
+				// 	venta_total.reduce((a, b) => a + b, 0),
+				// 	2,
+				// );
+				// const total_orden_final = fixedNumber(
+				// 	total_orden - (total_orden - gran_total_samishop),
+				// 	2,
+				// );
+				// const descuento_cupon = fixedNumber(
+				// 	(total_orden - gran_total_samishop) / cantidad.length,
+				// 	2,
+				// );
+
+				// const fecha_entrega = "";
+				// const pedido_sap = "";
+				// const codigo_vendedor = "";
+				// const comision_vendedor = "";
+				// const servicion_envio = formatText(
+				// 	path[0].datos_envio[0].servicio_envio,
+				// );
 
 				result.push({
 					NUMERO_ORDEN: numero_orden,
@@ -228,27 +261,21 @@ export async function transformData(datos_pedido) {
 					MAILING_PROMOCIONAL: mailing_promocional,
 					NOMBRE_COMPRADOR: nombre_comprador,
 					TELEFONO_COMPRADOR: telefono_comprador,
-					SKU: sku,
-					NOMBRE_PRODUCTO: nombre_producto,
-					CANTIDAD: cantidad,
 					MONEDA: moneda,
-					PRECIO_CATALOGO: precio_catalogo,
-					PRECIO_SALE: precio_sale,
-					DESCUENTO_CATALOGO: descuento_catalogo,
+					PRECIO_ENVIO_TOTAL: total_precio_envio,
+					LISTADO_PRODUCTOS: listadoProductos,
 					DESCUENTO_CONDICION: descuento_condicion,
-					PRECIO_UNITARIO: precio_unitario,
-					SUB_TOTAL: sub_total,
-					PRECIO_ENVIO: precio_envio,
-					VENTA_TOTAL: venta_total,
-					DESCUENTO_CUPON: descuento_cupon,
-					PRECIO_TOTAL_PROCESADO: precio_total_procesado,
-					TOTAL_DESCUENTO: total_descuento,
+					GRAN_TOTAL_API: gran_total_samishop,
+					GRAN_TOTAL: gran_total,
+					// DESCUENTO_CUPON: descuento_cupon,
 					NOMBRE_RECEPTOR: nombre_receptor,
 					DIRECCION: direccion,
 					DIRECCION_2: direccion2,
-					DISTRITO_PROVINCIA: distrito_provincia,
+					DISTRITO: distrito,
+					PROVINCIA: provincia,
 					DEPARTAMENTO: departamento,
 					UBIGEO: ubigeo,
+					DISTRITO_PROVINCIA: distrito_provincia,
 					PAIS: pais,
 					TELEFONO: telefono,
 					CODIGO_PROMOCION: codigo_promocion,
@@ -269,21 +296,21 @@ export async function transformData(datos_pedido) {
 					ID_PAGO: id_pago,
 					TIPO_DOCUMENTO: tipo_documento,
 					NUMERO_DOCUMENTO: numero_documento,
-					TOTAL_SIN_IGV: total_sin_igv,
-					PORCENTAJE_CUPON: porcentaje_cupon,
-					TOTAL_ORDEN: total_orden_final,
-					FECHA_ENTREGA: fecha_entrega,
-					PEDIDO_SAP: pedido_sap,
-					CODIGO_VENDEDOR: codigo_vendedor,
-					COMISION_VENDEDOR: comision_vendedor,
-					SERVICIO_ENVIO: servicion_envio,
-					//**************//
-					TOTAL_PRECIO_ENVIO: total_precio_envio,
-					TOTAL_ORDEN_PRE: total_orden,
-					DISTRITO: distrito,
-					PROVINCIA: provincia,
-					GRAN_TOTAL_SAMISHOP: gran_total_samishop,
-					GRAN_TOTAL: gran_total,
+					// // FINAL_TOTAL_SIN_IGV: final_total_sin_igv,
+					// // PORCENTAJE_CUPON: porcentaje_cupon,
+					// // TOTAL_ORDEN: total_orden_final,
+					// // FECHA_ENTREGA: fecha_entrega,
+					// // PEDIDO_SAP: pedido_sap,
+					// // CODIGO_VENDEDOR: codigo_vendedor,
+					// // COMISION_VENDEDOR: comision_vendedor,
+					// // SERVICIO_ENVIO: servicion_envio,
+					// //**************//
+					// TOTAL_PRECIO_ENVIO: total_precio_envio,
+					// // TOTAL_ORDEN_PRE: total_orden,
+					// DISTRITO: distrito,
+					// PROVINCIA: provincia,
+					// GRAN_TOTAL_SAMISHOP: gran_total_samishop,
+					// GRAN_TOTAL: gran_total,
 					//**************//
 				});
 			});
